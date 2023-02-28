7fcc3b526e20c1c41deeb51ba33f636534a3be60
AMRESH  MISHRA <121451346+amresh247@users.noreply.github.com>
2023-02-28 05:42:20 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,14 +1,14 @@
 import streamlit as st
 
 st.title(" :red[Innomatics] :white[Reasearch Lab] :red[Internship 2023]")
-st.caption("I thanks :blue[_Innomatics Reasearch Lab_] to giving me this oppotunity ")
+st.caption("I thanks :blue[_Innomatics Reasearch Lab_] to giving me this opportunity ")
 st.snow()
 
 btn_click = st.button("My Intro 👈 ")
 
 if btn_click == True:
     st.subheader("Myself Amresh Mishra , My qualification is MBA(IT and marekting) and BCA . \
-                 While doing Job in Indiamart(sales) for 1 years I realise i don't continue my studies with Job. \
+                 While doing Job in Indiamart(sales) for 1 years I realise that I am not able to giving my 100% to my studies while working \
                  So I left the job and Restarted my studies to make my career in Data Science field and dovoted myself \
                  to upgrade my skills . \
                  I also persuing Advance certifiction in Data Science from CCE IIT madras \
